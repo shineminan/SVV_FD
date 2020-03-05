a57595790c29f34e0f1073042d533e7d7c4d3c7a
shineminan <m.shi@student.tudelft.nl>
2020-03-05 17:31:33 +0100

    add the symmetric solver

--- main.py
+++ main.py
@@ -1,1 +1,44 @@
 # the main file
+w = 60500  # N, standard aicraft mass
+mdotfs = 0.048  # kg/second standard engine fuel flow per engine
+rho0 = 1.225 # kg/m3 standard air density
+
+lamda = -0.0065         # temperature gradient in ISA [K/m]
+Temp0  = 288.15          # temperature at sea level in ISA [K]
+R      = 287.05          # specific gas constant [m^2/sec^2K]
+g      = 9.81            # [m/sec^2] (gravity constant)
+
+S = 30  # area
+cbar = 2.0569  # MAC
+b = 15.911  # span
+
+cd0 = 0.04
+clalpha = 5.084
+e = 0.8
+kxx2 = 0.019
+kyy2 = 1.3925
+kzz2 = 0.042
+kxz = 0.002
+
+#  Linear model stability derivatives – symmetric flight
+cxu = -0.0279
+cxalpha = -0.4797
+cxalphadot = 0.0833
+cxq = -0.2817
+cxdelta = -0.0373
+
+czu = -0.3762
+czalpha = -5.7434
+czalphador = -0.0035
+czq = -5.6629
+czdelta = -0.6961
+
+cm0 = 0.0297
+cmu = 0.0699
+cmalpha = -0.5626
+cmalphadot = 0.1780
+cmq = -8.7941
+cmdelta = -1.1642
+cmtc = -0.0064
+
+miuc =
--- statespace.py
+++ statespace.py
@@ -1,1 +1,27 @@
-# the main file
+import numpy as np
+import Cit_par as CA
+
+def symmetric_motion_solver():
+	V = CA.V0
+	p = np.zeros((4, 4))
+	q = np.zeros((4, 4))
+	p[0, 0] = -2*CA.muc*CA.c/V
+	p[1, 1] = (CA.CZadot-2*CA.muc)*CA.c/V
+	p[2, 2] = -CA.c/V
+	p[3, 1] = CA.Cmadot*CA.c/V
+	p[3, 3] = -2*CA.muc*CA.KY2*CA.c/V
+	print(p)
+
+	q[0] = [-CA.CXu, -CA.CXa, -CA.CZ0, -CA.CXq]
+	q[1] = [-CA.CZu, -CA.CZa, CA.CX0, -(CA.CZq+2*CA.muc)]
+	q[2, -1] = -1
+	q[3] = [-CA.Cmu, -CA.Cma, 0, -CA.Cmq]
+	print(q)
+
+symmetric_motion_solver()
+
+
+
+
+
+
